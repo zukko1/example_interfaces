98275dbad0cde58845a2ad5bb92e159184c68906
jquiroz <jose.quiroz.serna@gmail.com>
2018-09-17 13:41:10 -0500

    example

--- public/index.php
+++ public/index.php
@@ -1,39 +1,54 @@
 <?php
 
-use App\Kernel;
-use Symfony\Component\Debug\Debug;
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\HttpFoundation\Request;
+interface CanFly {
+  public function fly();
+}
 
-require __DIR__.'/../vendor/autoload.php';
+interface CanSwim {
+  public function swim();
+}
 
-// The check is to ensure we don't use .env in production
-if (!isset($_SERVER['APP_ENV'])) {
-    if (!class_exists(Dotenv::class)) {
-        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
+class Bird {
+  public function info() {
+    echo "I am a {$this->name}\n";
+    echo "I am an bird\n";
+  }
+}
+
+class Dove {
+
+}
+
+class Penguin {
+
+}
+
+class Duck {
+
+}
+
+function describe($bird) {
+  if ($bird instanceof Bird) {
+    $bird->info();
+    if ($bird instanceof CanFly) {
+      $bird->fly();
     }
-    (new Dotenv())->load(__DIR__.'/../.env');
+    if ($bird instanceof CanSwim) {
+      $bird->swim();
+    }
+  } else {
+    echo "This is not a bird. I cannot describe it.";
+  }
 }
 
-$env = $_SERVER['APP_ENV'] ?? 'dev';
-$debug = (bool) ($_SERVER['APP_DEBUG'] ?? ('prod' !== $env));
 
-if ($debug) {
-    umask(0000);
 
-    Debug::enable();
-}
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-}
 
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts(explode(',', $trustedHosts));
-}
+describe(new Penguin);
+echo "---\n";
 
-$kernel = new Kernel($env, $debug);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+describe(new Dove);
+echo "---\n";
+
+describe(new Duck);
